85321e03aa0f490b0032e85ef2c8337a6176a7ec
Sippawit Thammawiset <sippathamm@gmail.com>
2023-09-28 03:19:58 +0700

    Initial Commit

---
+++ SortByName.py
@@ -0,0 +1,27 @@
+import csv
+
+date = '22-9-2023'
+data = []
+
+with open('output.csv') as f:
+    reader = csv.reader(f, delimiter=',')
+    skip_first_row = True
+    for row in reader:
+        if skip_first_row:
+            skip_first_row = False
+            continue
+        name, max_frequency, max_magnitude = row[0], row[1], row[2]
+        name = int(name.split('.')[0])
+        data.append([name, max_frequency, max_magnitude])
+
+data.sort()
+print(data)
+
+header = ['No.', 'Max Frequency (Hz)',	'Max Magnitude']
+
+with open(f'max_frequency_{date}.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(header)
+    writer.writerows(data)
+
